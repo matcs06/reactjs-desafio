6148eab3f21fb2dce6faaafc53219ff3c6d105ba
Mateus Costa <matcs06@gmail.com>
2020-04-12 15:51:24 -0300

    Adding frontEnd chalenge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  useEffect(() => {
+    api.get("/repositories").then((response) => setRepositories(response.data));
+  }, []);
+
   async function handleAddRepository() {
     // TODO
+    const response = await api.post("/repositories", {
+      title: `New repository ${Date.now()}`,
+      url: `www.newrepo.${Date.now()}`,
+      techs: ["React", "NodeJS", "React Native"],
+    });
+
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`/repositories/${id}`);
+    console.log(response.data);
+    setRepositories(repositories.filter((repo) => repo.id != id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repo) => {
+          return (
+            <div key={repo.id}>
+              <li>{repo.title}</li>
+              <button onClick={() => handleRemoveRepository(repo.id)}>
+                Remover
+              </button>
+            </div>
+          );
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
